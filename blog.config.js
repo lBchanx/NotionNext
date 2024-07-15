772a0c1b2ae2fc9d7bf7986241b6d567e7014fa5
lBchanx <124644533+lBchanx@users.noreply.github.com>
2024-07-15 21:54:24 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Rison Chan', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '东方欲晓，莫道君行晚。', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || '', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
