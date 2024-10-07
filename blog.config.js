9c60e69176e6340afba5c7322573243e5eea7217
lBchanx <124644533+lBchanx@users.noreply.github.com>
2024-10-07 16:35:11 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -25,7 +25,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Rison Chan', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '东方欲晓，莫道君行晚。', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '东方欲晓，莫道君行晚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || '', // 网站地址,链接在昵称实现
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '', // 网站关键词 英文逗号隔开
 
